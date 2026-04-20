d2afe0fbc3bf3821d55c8f7080ef8e02cb97f11f
GitHub Action <action@github.com>
2026-04-20 14:09:22 +0000

    Build and deploy to playground

---
+++ build/tab/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'd4c6e3364e6ecae58819');
